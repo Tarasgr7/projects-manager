83bc37ccdbf7d51bdd11147f2fa241967628fd9a
Tarasgr7 <tarashrickyv@gmail.com>
2025-02-12 16:59:55 +0200

    added connection to local DB

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -13,15 +13,27 @@ ALGORITHM = os.getenv("ALGORITHM")
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-def get_sync_db_url():
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    db = os.getenv("DB_NAME")
-    return f"postgresql://postgres:{password}@localhost:5432/{db}"
-
+#Якщо через докер
+# def get_sync_db_url():
+#     user = os.getenv("DOCKER_USER")
+#     password = os.getenv("DOCKER_PASSWORD")
+#     server=os.getenv("DOCKER_SERVER")
+#     db = os.getenv("DOCKER_NAME")
+#     return f"postgresql://{user}:{password}@{server}:5432/{db}"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("service")
+
+#Якщо через локальну БД
+def get_sync_db_url():
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    server=os.getenv("DB_SERVER")
+    db = os.getenv("DB_NAME")
+    logger.info(f"USERNAME:{user}")
+    return f"postgresql://{user}:{password}@{server}:5432/{db}"
+
+
 
 # Отримуємо URL бази даних із .env (або ставимо дефолтне значення)
 DATABASE_URL = get_sync_db_url()
